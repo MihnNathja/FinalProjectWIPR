c4f26768a5a5030eb97618f5ce9d86557c9e4718
MihnNathja <150262217+MihnNathja@users.noreply.github.com>
2024-02-15 10:40:58 +0700

    Create Nhat branch and add element to form đăng nhập

--- FinalProject/Form1.Designer.cs
+++ FinalProject/Form1.Designer.cs
@@ -29,45 +29,67 @@
         private void InitializeComponent()
         {
             this.btnDangNhap = new System.Windows.Forms.Button();
-            this.btnDangKi = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.btnDangKy = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnDangNhap
             // 
-            this.btnDangNhap.Location = new System.Drawing.Point(117, 321);
+            this.btnDangNhap.Location = new System.Drawing.Point(239, 301);
             this.btnDangNhap.Name = "btnDangNhap";
             this.btnDangNhap.Size = new System.Drawing.Size(126, 33);
             this.btnDangNhap.TabIndex = 0;
             this.btnDangNhap.Text = "Đăng nhập";
             this.btnDangNhap.UseVisualStyleBackColor = true;
             // 
-            // btnDangKi
+            // txtUserName
             // 
-            this.btnDangKi.Location = new System.Drawing.Point(316, 321);
-            this.btnDangKi.Name = "btnDangKi";
-            this.btnDangKi.Size = new System.Drawing.Size(126, 33);
-            this.btnDangKi.TabIndex = 1;
-            this.btnDangKi.Text = "Đăng kí";
-            this.btnDangKi.UseVisualStyleBackColor = true;
-            this.btnDangKi.Click += new System.EventHandler(this.button2_Click);
+            this.txtUserName.Location = new System.Drawing.Point(239, 150);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(291, 22);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(126, 156);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(76, 16);
+            this.lblUserName.TabIndex = 3;
+            this.lblUserName.Text = "User Name";
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Location = new System.Drawing.Point(404, 301);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(126, 33);
+            this.btnDangKy.TabIndex = 4;
+            this.btnDangKy.Text = "Đăng ký";
+            this.btnDangKy.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnDangKi);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.btnDangNhap);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnDangNhap;
-        private System.Windows.Forms.Button btnDangKi;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Button btnDangKy;
     }
 }
 
