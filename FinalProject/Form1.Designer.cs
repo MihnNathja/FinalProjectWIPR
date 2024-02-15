36fc82e6a5b75240404a80332707082ec360f339
VuongQue <lapque.hcmute@gmail.com>
2024-02-15 10:33:42 +0700

    Form đăng nhập

--- FinalProject/Form1.Designer.cs
+++ FinalProject/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.btnDangKi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Location = new System.Drawing.Point(117, 321);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(126, 33);
+            this.btnDangNhap.TabIndex = 0;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            // 
+            // btnDangKi
+            // 
+            this.btnDangKi.Location = new System.Drawing.Point(316, 321);
+            this.btnDangKi.Name = "btnDangKi";
+            this.btnDangKi.Size = new System.Drawing.Size(126, 33);
+            this.btnDangKi.TabIndex = 1;
+            this.btnDangKi.Text = "Đăng kí";
+            this.btnDangKi.UseVisualStyleBackColor = true;
+            this.btnDangKi.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDangKi);
+            this.Controls.Add(this.btnDangNhap);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Button btnDangKi;
     }
 }
 
