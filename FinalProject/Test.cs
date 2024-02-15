5771bcc92d762f96fad9a6d73a14c4c3fe9bd6cc
MihnNathja <150262217+MihnNathja@users.noreply.github.com>
2024-02-15 11:31:12 +0700

    Test

---
+++ FinalProject/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    internal class Test
+    {
+        int x;
+        int y;
+        int z = 0;
+        public Test() { }
+    }
+}
