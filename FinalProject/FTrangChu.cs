4a30390d128f31ebb6cf265edd3d5bc981f7ac28
VuongQue <lapque.hcmute@gmail.com>
2024-02-28 09:20:41 +0700

    Update FUngVien, Create FDoanhNghiep,FTrang Chu

---
+++ FinalProject/FTrangChu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class FTrangChu : Form
+    {
+        public FTrangChu()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
