639ed5e6a97b6407473911916ee44a055073c680
VuongQue <lapque.hcmute@gmail.com>
2024-02-28 09:04:48 +0700

    update FUngVien create FDoanhNghiep, FTrangChu

--- FinalProject/FUngVien.Designer.cs
+++ FinalProject/FUngVien.Designer.cs
@@ -28,47 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.flowLayoutPanel1.SuspendLayout();
+            this.btnKhaoSat = new System.Windows.Forms.Button();
+            this.btnCongViecQuanTam = new System.Windows.Forms.Button();
+            this.btnCongViecUngTuyen = new System.Windows.Forms.Button();
+            this.btnCV = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblCv = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.AutoSize = true;
-            this.flowLayoutPanel1.Controls.Add(this.groupBox1);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1286, 573);
-            this.flowLayoutPanel1.TabIndex = 1;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnKhaoSat);
+            this.groupBox1.Controls.Add(this.btnCongViecQuanTam);
+            this.groupBox1.Controls.Add(this.btnCongViecUngTuyen);
+            this.groupBox1.Controls.Add(this.btnCV);
+            this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 25);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(310, 569);
-            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Sinh Viên";
+            this.groupBox1.Text = "Ứng Viên";
+            // 
+            // btnKhaoSat
+            // 
+            this.btnKhaoSat.Location = new System.Drawing.Point(44, 442);
+            this.btnKhaoSat.Name = "btnKhaoSat";
+            this.btnKhaoSat.Size = new System.Drawing.Size(222, 38);
+            this.btnKhaoSat.TabIndex = 4;
+            this.btnKhaoSat.Text = "Khảo Sát";
+            this.btnKhaoSat.UseVisualStyleBackColor = true;
+            // 
+            // btnCongViecQuanTam
+            // 
+            this.btnCongViecQuanTam.Location = new System.Drawing.Point(44, 372);
+            this.btnCongViecQuanTam.Name = "btnCongViecQuanTam";
+            this.btnCongViecQuanTam.Size = new System.Drawing.Size(222, 38);
+            this.btnCongViecQuanTam.TabIndex = 3;
+            this.btnCongViecQuanTam.Text = "Công Việc Quan Tâm";
+            this.btnCongViecQuanTam.UseVisualStyleBackColor = true;
+            // 
+            // btnCongViecUngTuyen
+            // 
+            this.btnCongViecUngTuyen.Location = new System.Drawing.Point(44, 310);
+            this.btnCongViecUngTuyen.Name = "btnCongViecUngTuyen";
+            this.btnCongViecUngTuyen.Size = new System.Drawing.Size(222, 38);
+            this.btnCongViecUngTuyen.TabIndex = 2;
+            this.btnCongViecUngTuyen.Text = "Công Việc Ứng Tuyển";
+            this.btnCongViecUngTuyen.UseVisualStyleBackColor = true;
+            // 
+            // btnCV
+            // 
+            this.btnCV.Location = new System.Drawing.Point(44, 251);
+            this.btnCV.Name = "btnCV";
+            this.btnCV.Size = new System.Drawing.Size(222, 38);
+            this.btnCV.TabIndex = 1;
+            this.btnCV.Text = "CV";
+            this.btnCV.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(73, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblCv
+            // 
+            this.lblCv.AutoSize = true;
+            this.lblCv.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCv.Location = new System.Drawing.Point(367, 25);
+            this.lblCv.Name = "lblCv";
+            this.lblCv.Size = new System.Drawing.Size(136, 25);
+            this.lblCv.TabIndex = 2;
+            this.lblCv.Text = "Danh sách Cv";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(372, 101);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(857, 293);
+            this.dataGridView1.TabIndex = 6;
             // 
             // FUngVien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1286, 573);
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblCv);
+            this.Controls.Add(this.groupBox1);
             this.Name = "FUngVien";
             this.Text = "Form Ứng Viên";
-            this.flowLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnKhaoSat;
+        private System.Windows.Forms.Button btnCongViecQuanTam;
+        private System.Windows.Forms.Button btnCongViecUngTuyen;
+        private System.Windows.Forms.Button btnCV;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblCv;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
