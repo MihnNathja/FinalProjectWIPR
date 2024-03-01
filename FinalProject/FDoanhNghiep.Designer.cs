77ed6898d232d1618d5abc661aa1183b2aada522
VuongQue <lapque.hcmute@gmail.com>
2024-03-01 10:53:49 +0700

    Create FThemCongViec, Update Button themcongviec in FDoanhghiep

--- FinalProject/FDoanhNghiep.Designer.cs
+++ FinalProject/FDoanhNghiep.Designer.cs
@@ -34,10 +34,9 @@
             this.btnCongViecUngTuyen = new System.Windows.Forms.Button();
             this.btnCV = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -57,7 +56,7 @@
             // 
             // btnKhaoSat
             // 
-            this.btnKhaoSat.Location = new System.Drawing.Point(44, 442);
+            this.btnKhaoSat.Location = new System.Drawing.Point(44, 433);
             this.btnKhaoSat.Name = "btnKhaoSat";
             this.btnKhaoSat.Size = new System.Drawing.Size(222, 38);
             this.btnKhaoSat.TabIndex = 4;
@@ -99,28 +98,26 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // dataGridView1
+            // button1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(379, 110);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(693, 365);
-            this.dataGridView1.TabIndex = 5;
+            this.button1.Location = new System.Drawing.Point(918, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Thêm Công Việc";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // FDoanhNghiep
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1152, 597);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.dataGridView1);
             this.Name = "FDoanhNghiep";
             this.Text = "FDoanhNghiep";
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -133,6 +130,6 @@
         private System.Windows.Forms.Button btnCongViecUngTuyen;
         private System.Windows.Forms.Button btnCV;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
     }
 }
---
+++ FinalProject/FThemCongViec.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class FThemCongViec : Form
+    {
+        public FThemCongViec()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
